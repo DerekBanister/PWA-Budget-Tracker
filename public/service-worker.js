4a52008b604227cd362aecfc780df629a531d582
Derek Banister <ptownderek@yahoo.com>
2021-11-17 18:57:16 -0800

    setting up web manifest, service worker, and indexedDB

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-console.log("Service-Worker working");
+// console.log("Service-Worker working");
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
@@ -7,7 +7,11 @@ const FILES_TO_CACHE = [
     '/',
     '/index.html',
     '/manifest.json',
+    "favicon.ico",
+    "index.js",
     '/db.js',
+    "service-worker.js",
+    "manifest.webmanifest",
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
     '/styles.css'
